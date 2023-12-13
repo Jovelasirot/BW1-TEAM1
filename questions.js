d38534d972c52209714a42904f7f4d184b498687
Jovelasirot <jovelasirot@gmail.com>
2023-12-13 23:14:50 +0100

    timer restart when i click next question btn

--- questions.js
+++ questions.js
@@ -94,23 +94,25 @@ const questions = [
   },
 ];
 
-window.onload = function () {
-  // TIPS:
+// TIPS:
 
-  // SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
-  // Per ogni domanda, crea un container e incorporale tutte all'interno.
-  // Crea poi dei radio button
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-  // con le risposte corrette e incorrette come opzioni
-  // (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale)
-  //
-  // SE MOSTRI UNA DOMANDA ALLA VOLTA:
-  // Mostra la prima domanda con il testo e i radio button.
-  // Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
-  // salvando le risposte dell'utente in una variabile
+// SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
+// Per ogni domanda, crea un container e incorporale tutte all'interno.
+// Crea poi dei radio button
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
+// con le risposte corrette e incorrette come opzioni
+// (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale)
+//
+// SE MOSTRI UNA DOMANDA ALLA VOLTA:
+// Mostra la prima domanda con il testo e i radio button.
+// Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
+// salvando le risposte dell'utente in una variabile
+
+window.onload = function () {
   appendQuestion(0);
   appendAnswers(0);
   questionCurrentValue(0);
+  initializeTimer();
 };
 
 const appendQuestion = (n) => {
@@ -171,6 +173,8 @@ const checkUserAnswer = () => {
       nCorrect += 1;
     }
     n += 1;
+    clearInterval(timerLoop);
+    initializeTimer();
     console.log(nCorrect);
     appendQuestion(n);
     appendAnswers(n);
@@ -191,172 +195,52 @@ const checkUserAnswer = () => {
 let n = 0;
 let nCorrect = 0;
 
-// const FULL_DASH_ARRAY = 283;
-// const WARNING_THRESHOLD = 10;
-// const ALERT_THRESHOLD = 5;
+let timerLoop;
+let futureTime;
 
-// const COLOR_CODES = {
-//   info: {
-//     color: "green",
-//   },
-//   warning: {
-//     color: "orange",
-//     threshold: WARNING_THRESHOLD,
-//   },
-//   alert: {
-//     color: "red",
-//     threshold: ALERT_THRESHOLD,
-//   },
-// };
+const initializeTimer = () => {
+  const timer = document.querySelector(".timer");
+  const semicircle = document.querySelectorAll(".semicircle");
+  const sec = 4;
 
-// const timer = document.querySelector(".timer");
-// const semicircle = document.querySelectorAll(".semicircle");
-// const sec = 15;
+  const seconds = sec * 1000;
+  const setTime = seconds;
+  const starTime = Date.now();
+  futureTime = starTime + seconds;
 
-// const seconds = sec * 1000;
-// const setTime = seconds;
-// const starTime = Date.now();
-// const futureTime = starTime + seconds;
+  const countDownTimer = () => {
+    const currentTime = Date.now();
+    const remainingTime = futureTime - currentTime;
+    const angle = (remainingTime / setTime) * -360;
 
-// const countDownTimer = () => {
-//   const currentTime = Date.now();
-//   const remainingTime = futureTime - currentTime;
-//   const angle = (remainingTime / setTime) * -360;
+    if (angle > -180) {
+      semicircle[2].style.display = "none";
+      semicircle[0].style.transform = "rotate(-180deg)";
+      semicircle[1].style.transform = `rotate(${angle}deg)`;
+    } else {
+      semicircle[2].style.display = "block";
+      semicircle[0].style.transform = `rotate(${angle}deg)`;
+      semicircle[1].style.transform = `rotate(${angle}deg)`;
+    }
 
-//   if (angle > -180) {
-//     semicircle[2].style.display = "none";
-//     semicircle[0].style.transform = "rotate(-180deg)";
-//     semicircle[1].style.transform = `rotate(${angle}deg)`;
-//   } else {
-//     semicircle[2].style.display = "block";
-//     semicircle[0].style.transform = `rotate(${angle}deg)`;
-//     semicircle[1].style.transform = `rotate(${angle}deg)`;
-//   }
+    const secs = Math.floor((remainingTime / 1000) % 60);
 
-//   const secs = Math.floor((remainingTime / 1000) % 60);
+    timer.innerHTML = `
+    <div class="timerText">SECONDS</div>
+    <div>${secs < 10 ? "0" + secs : secs}</div>
+    <div class="timerText">REMAINING</div>`;
 
-//   timer.innerHTML = `
-//   <div class="timerText">SECONDS</div>
-//   <div>${secs < 10 ? "0" + secs : secs}</div>
-//   <div class="timerText">REMAINING</div>`;
-
-//   if (remainingTime < 10 * 1000) {
-//     timer.style.color = "red";
-//   }
-//   if (remainingTime < 0) {
-//     clearInterval(timerLoop);
-//     semicircle[0].style.display = "none";
-//     semicircle[1].style.display = "none";
-//     semicircle[2].style.display = "none";
-//     timer.innerHTML = `<div class="timerText">SECONDS</div>
-//    <div>00</div>
-//   <div class="timerText">REMAINING</div>`;
-//   } else {
-//   }
-// };
-
-// const timerLoop = setInterval(countDownTimer);
-
-// -----------------------------------------------------------------------------------------------------------
-
-// const TIME_LIMIT = 10;
-// let timePassed = 1;
-// let timeLeft = TIME_LIMIT;
-// let timerInterval = null;
-// let remainingPathColor = COLOR_CODES.info.color;
-
-// document.getElementById("app").innerHTML = `
-// <div class="base-timer">
-//   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-//     <g class="base-timer__circle">
-//       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-//       <path
-//         id="base-timer-path-remaining"
-//         stroke-dasharray="283"
-//         class="base-timer__path-remaining ${remainingPathColor}"
-//         d="
-//           M 50, 50
-//           m -45, 0
-//           a 45,45 0 1,0 90,0
-//           a 45,45 0 1,0 -90,0
-//         "
-//       ></path>
-//     </g>
-//     </svg>
-
-//     <span id="base-timer-label" class="base-timer__label">
-//     ${formatTime(timeLeft)}
-//   </span>
-// </div>
-// `;
-
-// function onTimesUp() {
-//   clearInterval(timerInterval);
-//   timerInterval = null;
-// }
-
-// function startTimer() {
-//   timeLeft = TIME_LIMIT;
-//   timePassed = 0;
-//   timerInterval = null;
-//   let baseTimerLabel = document.getElementById("base-timer-label");
-//   baseTimerLabel.innerHTML = formatTime(timeLeft);
-//   setCircleDasharray();
-//   timerInterval = setInterval(() => {
-//     timeLeft = TIME_LIMIT - timePassed;
-//     timePassed += 1;
-//     baseTimerLabel.innerHTML = formatTime(timeLeft);
-//     setCircleDasharray();
-//     setRemainingPathColor(timeLeft);
-
-//     if (timeLeft === 0) {
-//       n += 1;
-//       appendQuestion(n);
-//       appendAnswers(n);
-//       questionCurrentValue(n);
-//     }
-//   }, 1000);
-// }
-
-// function formatTime(time) {
-//   let seconds = time % 60;
-
-//   if (seconds < 10) {
-//     seconds = `${seconds}`;
-//   }
-
-//   return `${seconds}`;
-// }
-
-// function setRemainingPathColor(timeLeft) {
-//   const { alert, warning, info } = COLOR_CODES;
-//   if (timeLeft <= alert.threshold) {
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.remove(warning.color);
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.add(alert.color);
-//   } else if (timeLeft <= warning.threshold) {
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.remove(info.color);
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.add(warning.color);
-//   }
-// }
-
-// function calculateTimeFraction() {
-//   const rawTimeFraction = timeLeft / TIME_LIMIT;
-//   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-// }
-
-// function setCircleDasharray() {
-//   const circleDasharray = `${(
-//     calculateTimeFraction() * FULL_DASH_ARRAY
-//   ).toFixed(0)} 283`;
-//   document
-//     .getElementById("base-timer-path-remaining")
-//     .setAttribute("stroke-dasharray", circleDasharray);
-// }
+    if (remainingTime < 10 * 1000) {
+      timer.style.color = "red";
+    }
+    if (remainingTime <= 0) {
+      semicircle[0].style.display = "none";
+      semicircle[1].style.display = "none";
+      semicircle[2].style.display = "none";
+      timer.innerHTML = `<div class="timerText">SECONDS</div>
+     <div>00</div>
+    <div class="timerText">REMAINING</div>`;
+    }
+  };
+  const timerLoop = setInterval(countDownTimer);
+};
