5bba5f2079d9a51e6891f01840950ad100971d61
Fidan Hykolli <fidan-323@live.com>
2023-12-13 11:41:13 +0100

    questionPage +Target

--- questions.js
+++ questions.js
@@ -1,3 +1,123 @@
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green",
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD,
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD,
+  },
+};
+
+const TIME_LIMIT = 60;
+let timePassed = 1;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+    </svg>
+    
+   
+    <span id="base-timer-label" class="base-timer__label">
+    ${formatTime(timeLeft)} 
+  </span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+}
+
+function startTimer() {
+  timeLeft = TIME_LIMIT;
+  timePassed = 0;
+  timerInterval = null;
+  let baseTimerLabel = document.getElementById("base-timer-label");
+  baseTimerLabel.innerHTML = formatTime(timeLeft);
+  setCircleDasharray();
+  timerInterval = setInterval(() => {
+    timeLeft = TIME_LIMIT - timePassed;
+    timePassed += 1;
+    baseTimerLabel.innerHTML = formatTime(timeLeft);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      checkUserAnswer();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `${seconds}`;
+  }
+
+  return `${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
 const questions = [
   {
     category: "Science: Computers",
@@ -95,28 +215,19 @@ const questions = [
 ];
 
 window.onload = function () {
-  // TIPS:
-
-  // SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
-  // Per ogni domanda, crea un container e incorporale tutte all'interno.
-  // Crea poi dei radio button
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-  // con le risposte corrette e incorrette come opzioni
-  // (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale)
-  //
-  // SE MOSTRI UNA DOMANDA ALLA VOLTA:
-  // Mostra la prima domanda con il testo e i radio button.
-  // Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
-  // salvando le risposte dell'utente in una variabile
   appendQuestion(0);
   appendAnswers(0);
   questionCurrentValue(0);
 };
 
+//con la funzione appendQuestion prendo le domande dell'array questions
+
 const appendQuestion = (n) => {
   const onScreenQuestion = document.getElementById("question");
   onScreenQuestion.innerText = questions[n].question;
 };
+
+//con la funzione allTheAnswers pusho nell'array "answersArray" le risposte
 
 const allTheAnswers = (n) => {
   const answersArray = [];
@@ -132,7 +243,9 @@ const allTheAnswers = (n) => {
   return answersArray;
 };
 
-const piallaTutto = () => {
+//con la funzione resetBtnClick resetto la risposta cliccata in precedenza
+
+const resetBtnClick = () => {
   const piallato = document.getElementsByClassName("btn");
   for (let i = 0; i < piallato.length; i++) {
     piallato[i].classList.remove("btn-clicked");
@@ -148,12 +261,14 @@ const appendAnswers = (n) => {
     newDiv.innerText = answersArray[i];
     newDiv.classList.add("btn");
     newDiv.addEventListener("click", function (e) {
-      piallaTutto();
+      resetBtnClick();
       newDiv.classList.add("btn-clicked");
     });
     answersContainer.appendChild(newDiv);
   }
 };
+
+//Con la funzione questionCurrentValue prendo l'ID counter e ci scrivo il numero di domande, n = 1 ---> 1/10 , 2/10 , 3/10 ...
 
 const questionCurrentValue = (n) => {
   const currentValue = document.getElementById("counter");
@@ -161,27 +276,35 @@ const questionCurrentValue = (n) => {
 };
 
 const checkUserAnswer = () => {
-  if (n !== 9) {
+  if (n < 9) {
     let correctOne = questions[n].correct_answer;
-    let userAnswer;
-    userAnswer = document.getElementsByClassName("btn-clicked")[0].innerText;
-    console.log(userAnswer);
+
+    let userAnswer =
+      document.getElementsByClassName("btn-clicked") &&
+      document.getElementsByClassName("btn-clicked") > 0
+        ? document.getElementsByClassName("btn-clicked")[0].innerText
+        : "";
     if (userAnswer === correctOne) {
       nCorrect += 1;
     }
     n += 1;
-    console.log(nCorrect);
     appendQuestion(n);
     appendAnswers(n);
     questionCurrentValue(n);
+    onTimesUp();
+    startTimer();
   } else {
     let correctOne = questions[n].correct_answer;
-    let userAnswer;
-    userAnswer = document.getElementsByClassName("btn-clicked")[0].innerText;
+    let userAnswer =
+      document.getElementsByClassName("btn-clicked") &&
+      document.getElementsByClassName("btn-clicked") > 0
+        ? document.getElementsByClassName("btn-clicked")[0].innerText
+        : "";
     if (userAnswer === correctOne) {
       nCorrect += 1;
     }
     n += 1;
+
     window.location.assign("./resultsPage.html");
   }
 };
