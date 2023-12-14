53007ab27d29fe95786b6c053b4839fa46a45329
Ciro Vitiello <ciro.riccione@gmail.com>
2023-12-14 16:29:06 +0100

    final result 1.0

--- js/results.js
+++ js/results.js
@@ -1,29 +1,29 @@
-const resultsFromOtherPage = localStorage.getItem("myResults");
+const resultsFromOtherPage = localStorage.getItem('myResults')
 
-console.log(resultsFromOtherPage);
+console.log(resultsFromOtherPage)
 
 const modifyCorrectPercentage = () => {
-  const correctPercentage = document.getElementById("correct-percentage");
-  let userCorrectPercentage = (resultsFromOtherPage / 10) * 100;
+  const correctPercentage = document.getElementById('correct-percentage')
+  let userCorrectPercentage = (resultsFromOtherPage / 10) * 100
   correctPercentage.innerHTML = `       
   <span class="percentageFirst">Correct</span>
   <span class="percentageSecond">${userCorrectPercentage}%</span>
-  <span class="percentageThird">${resultsFromOtherPage}/10 questions </span>`;
-};
+  <span class="percentageThird">${resultsFromOtherPage}/10 questions </span>`
+}
 
 const modifyWrongPercentage = () => {
-  const wrongPercentage = document.getElementById("wrong-percentage");
-  let userWrongPercentage = ((10 - resultsFromOtherPage) / 10) * 100;
+  const wrongPercentage = document.getElementById('wrong-percentage')
+  let userWrongPercentage = ((10 - resultsFromOtherPage) / 10) * 100
   wrongPercentage.innerHTML = `       
   <span class="percentageFirst">Wrong</span>
   <span class="percentageSecond">${userWrongPercentage}%</span>
   <span class="percentageThird">${
     10 - resultsFromOtherPage
-  }/10 questions</span>`;
-};
+  }/10 questions</span>`
+}
 
 const passedOrNot = () => {
-  const passed = document.getElementById("passed");
+  const passed = document.getElementById('passed')
   if (resultsFromOtherPage >= 6) {
     passed.innerHTML = `<span id="congratTextCongrats">Congratulations! </span>
     <span id="congrat-textPassed">You passed the exam. <br />Polar bears survived.</span>
@@ -32,89 +32,80 @@ const passedOrNot = () => {
     in few minutes. <br />
     Check your email (incuding <br />
     promotions / spam folder)
-  </p>`;
+  </p>`
   } else if (resultsFromOtherPage > 4) {
-    passed.innerHTML = `Almost there! 
-    <span class="congrat-text">You were close to pass the exam. Only one polar bear got killed.</span>`;
+    passed.innerHTML = `<span class="notPassed">Almost there!</span><br />
+    <span class="notPassed-text">You were close to pass the exam. <br />Only a polar bear got killed.</span>`
   } else {
-    passed.innerHTML = `You failed! <br />
-    <span class="congrat-text">Study harder next time. Polar bears have died in vain.</span>`;
+    passed.innerHTML = `<span class="notPassed">You failed!</span><br />
+    <span class="notPassed-text">Study harder next time. Polar bears have died in vain.</span>`
   }
-};
+}
 
-const certificated = () => {
-  const certified = document.getElementById("certificate");
-  if (resultsFromOtherPage < 6) {
-    certified.innerHTML = ``;
-  }
-};
+modifyCorrectPercentage()
 
-modifyCorrectPercentage();
+modifyWrongPercentage()
 
-modifyWrongPercentage();
-
-passedOrNot();
-
-certificated();
+passedOrNot()
 
 // Donut JS
 
 window.chartColors = {
-  right: "rgb(210, 0, 148)",
-  wrong: "rgb(0, 255, 255)",
-};
+  right: 'rgb(210, 0, 148)',
+  wrong: 'rgb(0, 255, 255)',
+}
 
 Chart.defaults.global.tooltips.custom = function (tooltip) {
   // Tooltip Element
-  let tooltipEl = document.getElementById("chartjs-tooltip");
+  let tooltipEl = document.getElementById('chartjs-tooltip')
 
   // Hide if no tooltip
   if (tooltip.opacity === 0) {
-    tooltipEl.style.opacity = 0;
-    return;
+    tooltipEl.style.opacity = 0
+    return
   }
 
   // Set Text
   if (tooltip.body) {
-    let total = 0;
+    let total = 0
 
     // get the value of the datapoint
     let value =
       this._data.datasets[tooltip.dataPoints[0].datasetIndex].data[
         tooltip.dataPoints[0].index
-      ].toLocaleString();
+      ].toLocaleString()
 
     // calculate value of all datapoints
     this._data.datasets[tooltip.dataPoints[0].datasetIndex].data.forEach(
       function (e) {
-        total += e;
+        total += e
       }
-    );
+    )
 
     // calculate percentage and set tooltip value
-    tooltipEl.innerHTML = "<h1>" + (value / total) * 100 + "%</h1>";
+    tooltipEl.innerHTML = '<h1>' + (value / total) * 100 + '%</h1>'
   }
 
   // calculate position of tooltip
   let centerX =
     (this._chartInstance.chartArea.left + this._chartInstance.chartArea.right) /
-    2;
+    2
   let centerY =
     (this._chartInstance.chartArea.top + this._chartInstance.chartArea.bottom) /
-    2;
+    2
 
   // Display, position, and set styles for font
-  tooltipEl.style.opacity = 1;
-  tooltipEl.style.left = centerX + "px";
-  tooltipEl.style.top = centerY + "px";
-  tooltipEl.style.fontFamily = tooltip._fontFamily;
-  tooltipEl.style.fontSize = tooltip.fontSize;
-  tooltipEl.style.fontStyle = tooltip._fontStyle;
-  tooltipEl.style.padding = tooltip.yPadding + "px " + tooltip.xPadding + "px";
-};
+  tooltipEl.style.opacity = 1
+  tooltipEl.style.left = centerX + 'px'
+  tooltipEl.style.top = centerY + 'px'
+  tooltipEl.style.fontFamily = tooltip._fontFamily
+  tooltipEl.style.fontSize = tooltip.fontSize
+  tooltipEl.style.fontStyle = tooltip._fontStyle
+  tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px'
+}
 
 let config = {
-  type: "doughnut",
+  type: 'doughnut',
   data: {
     datasets: [
       {
@@ -136,9 +127,9 @@ let config = {
       enabled: false,
     },
   },
-};
+}
 
 window.onload = function () {
-  let ctx = document.getElementById("chart-area").getContext("2d");
-  window.myPie = new Chart(ctx, config);
-};
+  let ctx = document.getElementById('chart-area').getContext('2d')
+  window.myPie = new Chart(ctx, config)
+}
